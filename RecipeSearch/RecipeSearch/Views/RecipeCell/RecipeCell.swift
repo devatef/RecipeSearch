d8259032a108ec81eafa4d34aa365162d2e08e01
atefatia <devatef@gmail.com>
2020-01-08 23:24:26 +0200

    add basic structure

---
+++ RecipeSearch/RecipeSearch/Views/RecipeCell/RecipeCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeCell.swift
+//  RecipeSearch
+//
+//  Created by Atef on 1/8/20.
+//  Copyright © 2020 Atef. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
