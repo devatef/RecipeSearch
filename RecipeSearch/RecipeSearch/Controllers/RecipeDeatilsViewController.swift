b5e45c3a477697bf43223eb2f770f59b83d30a58
atefatia <devatef@gmail.com>
2020-01-13 01:16:23 +0200

    deatils controller

--- RecipeSearch/RecipeSearch/Controllers/RecipeDeatilsViewController.swift
+++ RecipeSearch/RecipeSearch/Controllers/RecipeDeatilsViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import UIKit
+import SafariServices
+import Kingfisher
 
 class RecipeDeatilsViewController: UIViewController {
+      @IBOutlet weak var titleLabel: UILabel!
+      @IBOutlet weak var ingridientsLabel: UILabel!
+      @IBOutlet weak var linkLabel: UILabel!
+      @IBOutlet weak var recipeImageView: UIImageView!
+      @IBOutlet weak var ingreideintsHeightConstraints: NSLayoutConstraint!
 
+    var recipeModel:Hit?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        recipeImageView.kf.setImage(with: URL(string:recipeModel?.recipe.image ?? ""), placeholder: UIImage(named: "default"))
+        titleLabel.text = recipeModel?.recipe.label
+        ingridientsLabel.text = recipeModel?.recipe.ingredientLines.joined(separator: "\n")
+        ingreideintsHeightConstraints.constant = CGFloat(25 * (recipeModel?.recipe.ingredientLines.count ?? 1))
+        linkLabel.text = recipeModel?.recipe.url
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func openSafariController() {
+        if let urlString = recipeModel?.recipe.url, let _url = URL(string: urlString) {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            let vc = SFSafariViewController(url: _url, configuration: config)
+            present(vc, animated: true)
+        }
+        
+     
+           
     }
     
 
